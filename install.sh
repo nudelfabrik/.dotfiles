6b58363c09bb770e09269e412c755a85c5f17e2f
Jess Scholtes <jess.scholtes@fourtytwo.xyz>
2013-04-10 16:20:31 +0200

    install.sh actualized

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # backup
 mkdir ~/dotfiles_backup 
-mv ~/.bash_profile ~/.vim ~/.vimrc ~/.gitconfig ~/.tmux.conf ~/.screenrc ~/dotfiles_backup
+mv ~/.bash_profile ~/.vim ~/.vimrc ~/.gitconfig ~/.tmux.conf ~/.screenrc ~/dotfiles_backup ~/.teamocil
 # make symlinks
 ln -s ~/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/vim ~/.vim
@@ -11,4 +11,5 @@ ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/screenrc ~/.screenrc
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/zshrc ~/.zshrc
+ln -s ~/.dotfiles/teamocil ~/.teamocil
 mkdir ~/.vimtmp
