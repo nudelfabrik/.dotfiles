faedc9235a2af9ad47fc4cfc563fbd0c4d7ef124
Jess Scholtes <jess.scholtes@fourtytwo.xyz>
2014-04-21 15:42:35 +0200

    remove screen

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ ln -s ~/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/screenrc ~/.screenrc
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/teamocil ~/.teamocil
