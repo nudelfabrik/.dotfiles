c3c84800eaede2944902a30aad7127a2d74f789e
Jess Scholtes <jess.scholtes@jscholtes.eu>
2014-05-30 18:38:12 +0200

    updated cd... commands

--- zsh/lib/directories.zsh
+++ zsh/lib/directories.zsh
@@ -2,3 +2,9 @@
 setopt auto_name_dirs
 setopt auto_pushd
 setopt pushd_ignore_dups
+
+alias ..='cd ..'
+alias cd..='cd ..'
+alias cd...='cd ../..'
+alias cd....='cd ../../..'
+alias cd.....='cd ../../../..'
