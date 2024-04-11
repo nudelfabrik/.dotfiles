bbc4170bc8de663d2998164288f92fc03f452749
Jess Scholtes <jess.scholtes@jscholtes.eu>
2024-04-11 09:28:34 +0200

    add better terraform syntax

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -40,7 +40,7 @@ require("lazy").setup({
             local configs = require("nvim-treesitter.configs")
 
             configs.setup({
-                ensure_installed = { "vim", "vimdoc", "markdown" },
+                ensure_installed = { "vim", "vimdoc", "markdown", "terraform" },
                 sync_install = false,
                 highlight = { enable = true },
                 indent = { enable = true },
