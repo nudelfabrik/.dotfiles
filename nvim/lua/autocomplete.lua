efdedd4b78d53802fa026fd635081e66b07647e0
Jess Scholtes <jess.scholtes@jscholtes.eu>
2024-07-09 15:31:46 +0200

    add yaml linter

--- nvim/lua/autocomplete.lua
+++ nvim/lua/autocomplete.lua
@@ -15,7 +15,7 @@ local lspconfig = require('lspconfig')
 --
 
 -- LSPs without any further config
-local servers = { 'clangd', 'cmake', 'gopls', 'pylsp', 'terraformls', 'jdtls' }
+local servers = { 'clangd', 'cmake', 'gopls', 'pylsp', 'terraformls', 'jdtls', 'yamlls' }
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
         -- on_attach = my_custom_on_attach,
