3575d5a13bb43a10381777497b93df3a20eff555
Jess Scholtes <jess.scholtes@jscholtes.eu>
2024-07-10 22:04:21 +0200

    add trailing-whitespace fix

--- nvim/lua/filetypes.lua
+++ nvim/lua/filetypes.lua
@@ -1,4 +1,12 @@
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-  pattern = {"*.rpz", "*.zone"},
-  callback = function() vim.opt.filetype="bindzone" end,
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+    pattern = { "*.rpz", "*.zone" },
+    callback = function() vim.opt.filetype = "bindzone" end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWrite" }, {
+    callback = function()
+        save_cursor = vim.fn.getpos(".")
+        vim.cmd([[%s/\s\+$//e]])
+        vim.fn.setpos(".", save_cursor)
+    end,
 })
