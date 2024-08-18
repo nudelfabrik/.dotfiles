9f2fea88f9ef3fd5364f8af1732aea2d2f750f5e
Jess Scholtes <jess.scholtes@jscholtes.eu>
2024-08-18 23:07:31 +0200

    add tree command for change node

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -115,6 +115,7 @@ local function my_on_attach(bufnr)
 
     -- custom mappings
     vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
+    vim.keymap.set('n', 'C', api.tree.change_root_to_node, opts('CD'))
     vim.keymap.set('n', 's', api.node.open.vertical, opts('Open: Vertical Split'))
 end
 
