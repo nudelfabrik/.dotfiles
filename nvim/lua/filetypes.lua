c7d8b9fc64f77f681f29ba169a100136fefe199f
Jess Scholtes <jess.scholtes@jscholtes.eu>
2025-06-22 21:37:23 +0200

    upgrade filetype detection

--- nvim/lua/filetypes.lua
+++ nvim/lua/filetypes.lua
@@ -1,11 +1,25 @@
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-    pattern = { "*.rpz", "*.zone" },
-    callback = function() vim.opt.filetype = "bindzone" end,
+vim.filetype.add({
+    extension = {},
+    filename = {},
+    pattern = {
+        ['.*/*.rpz'] = 'bindzone',
+        ['.*/*.sample'] = function(path, bufnr, ext)
+            local filename = string.sub(ext, 1, -8)
+            local match = vim.filetype.match({ filename = filename })
+            if match == nil then
+                local i, _ = string.find(filename, "%.")
+                if i ~= nil then
+                    match = string.sub(filename, i + 1)
+                end
+            end
+            return match
+        end
+    },
 })
 
 vim.api.nvim_create_autocmd({ "BufWrite" }, {
     callback = function()
-        save_cursor = vim.fn.getpos(".")
+        local save_cursor = vim.fn.getpos(".")
         vim.cmd([[%s/\s\+$//e]])
         vim.fn.setpos(".", save_cursor)
     end,
